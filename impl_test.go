8678a56479fb603c3e3bbc5be87a3fbfd3a8cb97
Yau <40264497+yautze@users.noreply.github.com>
2023-01-10 23:48:05 +0800

    fix: pr comment

--- impl_test.go
+++ impl_test.go
@@ -9,7 +9,7 @@ import (
 
 type CacheSuite struct {
 	suite.Suite
-	cache Cache
+	cache *cache
 }
 
 func TestCacheSuite(t *testing.T) {
@@ -17,7 +17,7 @@ func TestCacheSuite(t *testing.T) {
 }
 
 func (cs *CacheSuite) SetupTest() {
-	cs.cache = New()
+	cs.cache = New().(*cache)
 }
 
 func (cs *CacheSuite) TestSet() {
@@ -50,66 +50,92 @@ func (cs *CacheSuite) TestSet() {
 			tc.Setup()
 		}
 
-		res, _ := cs.cache.Get(tc.Key)
-		cs.Require().Equal(tc.Expect, res, tc.Desc)
+		res, _ := cs.cache.storage[tc.Key]
+		cs.Require().Equal(tc.Expect, res.value, tc.Desc)
 	}
 }
 
 func (cs *CacheSuite) TestGet() {
+	defaultSetup := func(key string, val interface{}) {
+		cs.cache.m.Lock()
+		defer cs.cache.m.Unlock()
+
+		cs.cache.storage[key] = &data{
+			value: val,
+			expiredHandle: time.AfterFunc(defaultExpiredTime, func() {
+				cs.cache.del(key)
+			}),
+		}
+	}
+
 	testCases := []struct {
-		Desc string
-		Key  string
-		Data interface{}
+		Desc  string
+		Key   string
+		Err   error
+		Data  interface{}
+		Setup func(string, interface{})
 	}{
 		{
 			Desc: "string type",
 			Key:  "string",
+			Err:  nil,
 			Data: "string",
 		},
 		{
 			Desc: "int type",
 			Key:  "int",
+			Err:  nil,
 			Data: 1,
 		},
 		{
 			Desc: "bool type",
 			Key:  "bool",
+			Err:  nil,
 			Data: true,
 		},
 		{
 			Desc: "array type",
 			Key:  "array",
+			Err:  nil,
 			Data: []int{1, 2, 3, 4},
 		},
 		{
 			Desc: "object type",
 			Key:  "object",
+			Err:  nil,
 			Data: map[string]string{"test": "test"},
+		},
+		{
+			Desc: "data not found error",
+			Key:  "not found",
+			Err:  ErrDataNotFound,
+			Data: nil,
+			Setup: func(key string, val interface{}) {
+			},
+		},
+		{
+			Desc: "data not found error cause by cache expired",
+			Key:  "cache expired",
+			Err:  ErrDataNotFound,
+			Data: nil,
+			Setup: func(key string, val interface{}) {
+				defaultExpiredTime = 10 * time.Millisecond
+				defaultSetup(key, val)
+				time.Sleep(20 * time.Millisecond)
+			},
 		},
 	}
 
 	for _, tc := range testCases {
-		cs.cache.Set(tc.Key, tc.Data)
+		switch {
+		case tc.Setup != nil:
+			tc.Setup(tc.Key, tc.Data)
+		default:
+			defaultSetup(tc.Key, tc.Data)
+		}
 
-		res, _ := cs.cache.Get(tc.Key)
+		res, err := cs.cache.Get(tc.Key)
 		cs.Require().Equal(tc.Data, res, tc.Desc)
+		cs.Require().Equal(tc.Err, err, tc.Desc)
 	}
-}
-
-func (cs *CacheSuite) TestDataNotFound() {
-	key := "Not Found"
-	res, err := cs.cache.Get(key)
-	cs.Require().Equal(nil, res, "not found")
-	cs.Require().Equal(ErrDataNotFound, err, "check return error")
-}
-
-func (cs *CacheSuite) TestCacheExpired() {
-	defaultExpiredTime = 10 * time.Millisecond
-	key := "test"
-
-	cs.cache.Set(key, 1)
-	time.Sleep(20 * time.Millisecond)
-
-	res, _ := cs.cache.Get(key)
-	cs.Require().Equal(nil, res, "cache expired")
 }
